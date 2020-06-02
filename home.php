4f334a77ce5198c3f045ae8de5ba2148200e921f
Juanpaab <xjuanpaab@gmail.com>
2020-06-01 19:48:43 -0500

    Buf fix
    Bug: Navbar won't close when in dashboard.
    Fix: Scripts include in home.php.
    <?php include 'includes/scripts.php'; ?>

---
+++ home.php
@@ -0,0 +1,86 @@
+<?php 
+  include 'includes/session.php';
+  include 'includes/format.php'; 
+?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <?php include 'includes/navbar.php'; ?>
+  <?php include 'includes/menubar.php'; ?>
+
+  <div class="content-wrapper">
+    <section class="content-header">
+      <h1>
+        Panel de control
+      </h1>
+    </section>
+
+    <section class="content">
+      <?php
+        if(isset($_SESSION['error'])){
+          echo "
+            <div class='alert alert-danger alert-dismissible'>
+              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+              <h4><i class='icon fa fa-warning'></i> !Error!</h4>
+              ".$_SESSION['error']."
+            </div>
+          ";
+          unset($_SESSION['error']);
+        }
+        if(isset($_SESSION['success'])){
+          echo "
+            <div class='alert alert-success alert-dismissible'>
+              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+              <h4><i class='icon fa fa-check'></i> !Exito!</h4>
+              ".$_SESSION['success']."
+            </div>
+          ";
+          unset($_SESSION['success']);
+        }
+      ?>
+      <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <div class="small-box bg-green">
+            <div class="inner">
+              <?php
+                $stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM products");
+                $stmt->execute();
+                $prow =  $stmt->fetch();
+
+                echo "<h3>".$prow['numrows']."</h3>";
+              ?>
+              <p>Número de productos</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-barcode"></i>
+            </div>
+            <a href="products.php" class="small-box-footer">Más información <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+          <div class="small-box bg-blue">
+            <div class="inner">
+              <?php
+                $stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM users");
+                $stmt->execute();
+                $urow =  $stmt->fetch();
+
+                echo "<h3>".$urow['numrows']."</h3>";
+              ?>
+             
+              <p>Número de usuarios</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-users"></i>
+            </div>
+            <a href="users.php" class="small-box-footer">Más información <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+      </div>
+      </section>
+    </div>
+    <?php include 'includes/footer.php'; ?>
+    <?php include 'includes/scripts.php'; ?>
+</body>
+</html>
